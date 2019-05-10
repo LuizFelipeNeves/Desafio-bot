48c349fa7a1695b2367691ad8e5264f5ab74654b
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-05-10 15:30:29 -0300

    *

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
-const sleep = require('sleep');
-const bd = require('./modulos/bd'); 
+const http = require('http');
 
-// não usei socket.io para realtime.
+require('./modulos/api');
 
-async function update(page, limit) {
-    const data = await bd.Listdb({}, page, limit);
-    for (let index = 0; index < data.length; index++) await bd.UpdateCPU(data[index]._id, {status : await bd.RandomStatus()});
-    if (await data.length >= limit) await update(++page, limit);    
-}
-
-async function exec() {
-    /*
-    await bd.NewStatus('01', 'Um');
-    await bd.NewStatus('02', 'Dois');
-    await bd.NewStatus('03', 'Tres');
-    await bd.NewStatus('04', 'Quatro');
-    await bd.NewStatus('05', 'Cinco');
-
-    for (let index = 0; index < data.length; index++) await bd.NewCPU({ nome: 'Branco ' + index, status: await bd.RandomStatus() });
-    */
-
-    await update(1, 50);
-    await sleep.sleep(await bd.GetTime());
-    await exec()
-}
-(async () => await exec())();
+// don`t sleep heroku, get every 5 minutes (300000)
+//setInterval(() => { http.get('http://pcsdevapi.herokuapp.com'); }, 300000);
