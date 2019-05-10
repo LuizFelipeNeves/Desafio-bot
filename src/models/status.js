6bbbaf01bba585a87fc88440b74a277fb50af3ce
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-05-10 15:29:29 -0300

    AutoIncrement

--- src/models/status.js
+++ src/models/status.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
 const StatusSchema = new mongoose.Schema({
+    id: Number,
     codigo: { type: String, required: true },
     nome: { type: String, required: true },
- });
+ }, { _id: false, timestamps: { createdAt: 'created_at' } });
 
+StatusSchema.plugin(AutoIncrement,{ id: 'status', inc_field: '_id'});
 module.exports = mongoose.model('Status', StatusSchema);
--- src/models/time.js
+++ src/models/time.js
@@ -1,4 +1,11 @@
-const mongoose = require('mongoose');
-const TimeSchema = new mongoose.Schema({ time: Number });
+const mongoose = require('mongoose')
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
+const TimeSchema = new mongoose.Schema({
+    _id: Number,
+    time: Number 
+}, { _id: false, timestamps: { createdAt: 'created_at' } });
+
+TimeSchema.plugin(AutoIncrement,{ id: 'time', inc_field: '_id'});
 module.exports = mongoose.model('Time', TimeSchema);
+
